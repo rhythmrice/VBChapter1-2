0f8b90c80f577baa537ace02da1d62f362c39122
rhythmrice <ricerhythm@gmail.com>
2017-11-01 09:41:13 -0500

    update new KiloConvertor

---
+++ kilo converter/kilo converter/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.btnInches = New System.Windows.Forms.Button()
+        Me.btnFeet = New System.Windows.Forms.Button()
+        Me.btnYards = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.Location = New System.Drawing.Point(219, 54)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(39, 13)
+        Me.lblMessage.TabIndex = 0
+        Me.lblMessage.Text = "Label1"
+        '
+        'btnInches
+        '
+        Me.btnInches.Location = New System.Drawing.Point(12, 181)
+        Me.btnInches.Name = "btnInches"
+        Me.btnInches.Size = New System.Drawing.Size(75, 23)
+        Me.btnInches.TabIndex = 1
+        Me.btnInches.Text = "Inches"
+        Me.btnInches.UseVisualStyleBackColor = True
+        '
+        'btnFeet
+        '
+        Me.btnFeet.Location = New System.Drawing.Point(127, 181)
+        Me.btnFeet.Name = "btnFeet"
+        Me.btnFeet.Size = New System.Drawing.Size(75, 23)
+        Me.btnFeet.TabIndex = 2
+        Me.btnFeet.Text = "Feet"
+        Me.btnFeet.UseVisualStyleBackColor = True
+        '
+        'btnYards
+        '
+        Me.btnYards.Location = New System.Drawing.Point(274, 181)
+        Me.btnYards.Name = "btnYards"
+        Me.btnYards.Size = New System.Drawing.Size(75, 23)
+        Me.btnYards.TabIndex = 3
+        Me.btnYards.Text = "Yards"
+        Me.btnYards.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(387, 181)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(199, 260)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(474, 373)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnYards)
+        Me.Controls.Add(Me.btnFeet)
+        Me.Controls.Add(Me.btnInches)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblMessage As System.Windows.Forms.Label
+    Friend WithEvents btnInches As System.Windows.Forms.Button
+    Friend WithEvents btnFeet As System.Windows.Forms.Button
+    Friend WithEvents btnYards As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
