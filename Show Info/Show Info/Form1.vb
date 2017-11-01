ede6db57f45e01f810e9a1fd835c08d8728f21cc
rhythmrice <ricerhythm@gmail.com>
2017-11-01 10:24:35 -0500

    show info complete

---
+++ Show Info/Show Info/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnShowinfo_Click(sender As Object, e As EventArgs) Handles btnShowinfo.Click
+        Label1.Visible = True
+    End Sub
+End Class
